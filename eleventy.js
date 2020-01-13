cb954323a7b31c2b5350787be7b7f1eb22be55d5
Matt O'Leary <mattosurf@gmail.com>
2020-01-12 22:44:16 -0800

    adding more shit

--- eleventy.js
+++ eleventy.js
@@ -11,4 +11,9 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.setTemplateFormats("html,liquid,njk,md,jpg,png,css");
   };
 
+  return {
+    
+    templateFormats: ["njk", "md", "jpg"],
+  
+  };
   
