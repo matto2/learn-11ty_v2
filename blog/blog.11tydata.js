8f88da72c753dc3d182e8d5113d087fc09d6afe5
Matt O'Leary <mattosurf@gmail.com>
2020-01-17 14:06:16 -0800

    changes

--- blog/blog.11tydata.js
+++ blog/blog.11tydata.js
@@ -1,10 +1,15 @@
+const axios = require('axios');
+
+
 module.exports = async () => {
 
-const result = await
+const result = await axios.get('https://dog.ceo/api/breeds/image/random');
 
+const image = result.data.message;
 
 return {
     layout: 'layout.liquid',
-    tags: 'blog'
+    tags: 'blog',
+    dog: image
 };
 }
--- blog/post2/index.md
+++ blog/post2/index.md
@@ -11,12 +11,3 @@ whassup??~~~~~~~~~~~
 
 {{ github.stargazers }} GitHub Stars
 
-{% raw %}
-<ul>  
-  {% for meetup in meetups %}  
-  <li>
-    <a href="{{ meetup.link }}">{{ meetup.name }}</a>
-  </li>  
-  {% endfor %}  
-</ul>
-{% endraw %}
